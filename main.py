5b38c4232b5804cf8613a5e143f741d6f6703ab6
RealGingerMcFerren <86018761+RealGingerMcFerren@users.noreply.github.com>
2021-06-16 14:27:22 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+import time
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+        await client.change_presence(activity=discord.Game('Powering On...'))
+        time.sleep(5)
+        await client.change_presence(activity=discord.Game('Waiting on Gingers Devices...'))
+        time.sleep(3)
+        await client.change_presence(activity=discord.Game('Logging in...'))
+        time.sleep(10)
+        await client.change_presence(activity=discord.Game('Ready to help!'))
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'E':
+            await message.channel.send('E')
+        if message.content == 'Hey System, Update.':
+            await message.channel.send('System 12.6\nIt looks like I\'m already updated.')
+
+
+
+
+
+
+
+
+
+
+client = MyClient()
+client.run('ODUyMjQ2NTM3NzMxMzA5NjA4.YMECcA.RKywCNPTcIaEOkcf3-l4dqwGT0Q')
